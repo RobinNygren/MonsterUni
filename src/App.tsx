13121dd7989a7a8f3dc767dc3e3cb025ff601262
Robin <robin_nygren@outlook.com>
2024-04-21 22:32:48 +0200

    added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./components/Layout/Header";
 import MyModal from "./components/UI/Modal";
 
 function App() {
   return (
     <>
+      <Header />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <MyModal />
     </>
---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-500 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-lg font-bold">MonstersUniversity</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
