dbd2518bed94166b8548d2e348c94d6e2153017d
Robin <robin_nygren@outlook.com>
2024-04-23 13:55:15 +0200

    added reducer

---
+++ src/state/reducer.ts
@@ -0,0 +1,27 @@
+import { Action, GlobalState } from "../types";
+
+const reducer = (state: GlobalState, action: Action): GlobalState => {
+  switch (action.type) {
+    case "ADD_MONSTERS":
+      return {
+        ...state,
+        monsters: [...state.monsters, action.payload],
+      };
+    case "REMOVE_MONSTER":
+      return {
+        ...state,
+        monsters: state.monsters.filter(
+          (monster) => monster.id !== action.payload.id
+        ),
+      };
+    case "UPDATE_MONSTER":
+      return {
+        ...state,
+        monsters: state.monsters.map((monster) =>
+          monster.id === action.payload.id ? action.payload : monster
+        ),
+      };
+    default:
+      return state;
+  }
+};
